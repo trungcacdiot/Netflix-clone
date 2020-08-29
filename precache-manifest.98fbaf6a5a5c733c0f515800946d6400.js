d9a5ab17eef517402cbd52502ac30b1fdd873868
trungbui <buibatrung01@gmail.com>
2020-08-29 20:12:44 +0700

    Updates

---
+++ precache-manifest.98fbaf6a5a5c733c0f515800946d6400.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ac2c43173366993f64d1164bcebb4d7",
+    "url": "/Netflix-clone/index.html"
+  },
+  {
+    "revision": "438b96188594de42c02b",
+    "url": "/Netflix-clone/static/css/main.0a56891d.chunk.css"
+  },
+  {
+    "revision": "c4df397e8c58f04f2d35",
+    "url": "/Netflix-clone/static/js/2.ebc10fe1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Netflix-clone/static/js/2.ebc10fe1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "438b96188594de42c02b",
+    "url": "/Netflix-clone/static/js/main.1fcad0b6.chunk.js"
+  },
+  {
+    "revision": "37005dd9bfd48c744798",
+    "url": "/Netflix-clone/static/js/runtime-main.2122e3aa.js"
+  }
+]);
